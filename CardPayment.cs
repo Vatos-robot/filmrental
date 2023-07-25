c13a2e7afe2fe366b56954a76559c0dedcf2dfad
hugobarbosa <hugodu38@icloud.com>
2023-07-25 16:21:04 +0200

    save work

--- CardPayment.cs
+++ CardPayment.cs
@@ -2,7 +2,7 @@ public class CardPaymentStrategy : IPaymentStrategy
 {
     public void Pay(double amount)
     {
-        
+        // Ici, vous pouvez mettre la logique de paiement par carte de crédit
         Console.WriteLine($"Paiement de {amount}€ effectué avec succès par carte de crédit.");
     }
 }
